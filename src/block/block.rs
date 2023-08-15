fe97d8998c028147dfa342901520013e21e270e7
nplayer89 <n.player89@gmail.com>
2023-08-15 10:46:56 -0600

    add block struct

---
+++ src/block/block.rs
@@ -0,0 +1,13 @@
+pub struct App {
+    pub blocks: Vec,
+}
+
+#[derive(Serialize, Deserialize, Debug, CLone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub prev_hash: String,
+    pub timestamp: i64,
+    pub data: string,
+    pub nonce: u64,
+}
