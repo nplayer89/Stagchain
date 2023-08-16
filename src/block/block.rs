69c5412d319711a92bce4b8b8647e4ca7d2f9090
nplayer89 <n.player89@gmail.com>
2023-08-16 10:35:06 -0600

    add function check if block is valid

--- src/block/block.rs
+++ src/block/block.rs
@@ -1,3 +1,14 @@
+const DIFFICULTY_PREFIX: &str = "00";
+
+fn hash_to_binary(hash: &[u8]) -> String {
+    let mut res: String = String::default();
+    for c in hash {
+        res.push_str(&format!("{:b}",c));
+    }
+    res
+}
+
+
 pub struct App {
     pub blocks: Vec,
 }
@@ -13,10 +24,12 @@ pub struct Block {
 }
 
 impl App {
+    // constructor
     fn new() -> Self {
         Self { blocks: vec![] }
     }
 
+    // create genesis block
     fn genesis(&mut self) {
         let genesis_block = Block {
             id: 0,
@@ -26,6 +39,44 @@ impl App {
             hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
         };
         self.blocks.push(genesis_block);
+    }
+
+    // attempt to add block to chain
+    fn try_add_block(&mut self, block: Block) {
+        let latest_block = self.blocks.last().expect("there is at least one block");
+        if self.is_block_valid(&block, latest_block){
+            self.blocks.push(block);
+        }
+        else {
+            error!("could not add block - invalid");
+        }
+    }
+
+    // check if block is valid
+    fn is_block_valid(&self, block: &Block, previous_block: &Block) -> bool {
+        if block.previous_hash != previous_block.hash {
+            warn!("block with id: {} has wrong previous hash", block.id);
+            return false;
+        } else if !hash_to_binary_representation(&hex::decode(&block.hash).expect("can decode from hex")).starts_with(DIFFICULTY_PREFIX){
+            warn!("block with id: {} has invalid difficulty", block.id);
+            return false;
+        } else if block.id != previous_block.id + 1 {
+            warn!(
+                "block with id: {} is not the next block after the latest: {}",
+                block.id, previous_block.id
+            );
+            return false;
+        } else if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.prev_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash {
+            warn!("block with id: {} has invalid hash", block.id);
+            return false;
+        }
+        true
     }
 }
 
