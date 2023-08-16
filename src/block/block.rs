031107375e2a2e57eca8692b0a95ec961aabe9f3
nplayer89 <n.player89@gmail.com>
2023-08-16 10:06:33 -0600

    add function to create genesis block

--- src/block/block.rs
+++ src/block/block.rs
@@ -16,4 +16,17 @@ impl App {
     fn new() -> Self {
         Self { blocks: vec![] }
     }
+
+    fn genesis(&mut self) {
+        let genesis_block = Block {
+            id: 0,
+            timestamp: Utc::now().timestamp(),
+            data: String::from("genesis!"),
+            nonce: 2836,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
+        };
+        self.blocks.push(genesis_block);
+    }
 }
+
+
