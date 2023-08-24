65d0b4f17505813f124e7d1c8345e442e6335a4f
nplayer89 <n.player89@gmail.com>
2023-08-24 12:17:47 -0600

    added code to validate chain

--- src/block/block.rs
+++ src/block/block.rs
@@ -78,6 +78,21 @@ impl App {
         }
         true
     }
+
+    //validate chain is valid
+    fn is_chain_valid(&self, chain: &[Block]) -> bool {
+        for i in 0..chain.len() {
+            if i == 0 {
+                continue;
+            }
+            let first = chain.get(i - 1).expect("has to exist");
+            let second = chain.get(i).expect("has to exist");
+            if !self.is_block_valid(second, first) {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 
